5118c8b8afd05a9bb3f54f468f22970b6bd17bf4
Vishv Soni <133687722+vishvsoni27@users.noreply.github.com>
2024-05-18 14:02:06 +0530

    Create 001-Largest-Element-in-Array.cpp

---
+++ 01-Arrays/001-Largest-Element-in-Array.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+         int  largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if(arr[i]>largest){
+            largest =arr[i];
+        }
+    }
+    return largest;
+    }
+};
